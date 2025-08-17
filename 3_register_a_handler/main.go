2e7ffbe6b314200ba4b1a9417338d1b73ff64259
Inasa <me@inasa.dev>
2025-08-17 13:39:22 +0800

    Register a handler

---
+++ 3_register_a_handler/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+
+	"go.uber.org/fx"
+)
+
+type EchoHandler struct{}
+
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (*EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+	}
+}
+
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+func main() {
+
+	fx.New(
+		fx.Provide(
+			NewHTTPServer,
+			NewServeMux, // 请注意，NewServeMux 被添加到 NewEchoHandler 的上方--向 fx.Provide 提供构造函数的顺序并不重要。
+			NewEchoHandler,
+		),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
+}
+
+// curl -X POST -d "你好，这是一个测试！" http://localhost:8080/echo
